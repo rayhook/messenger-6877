bd4971b8db074b0acd847ea233648dcd304f1df5
rayhook <koohyar@gmail.com>
2021-10-09 02:26:46 +0200

    Add new typography, palette and spacing props

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,47 +1,60 @@
 import { createTheme } from "@material-ui/core";
 
 export const theme = createTheme({
+  palette: {
+    primary: { main: "#3A8DFF" },
+    secondary: { main: "#8c8c8c" },
+    bright: { main: "#ffffff" }
+  },
   typography: {
     fontFamily: "Open Sans",
-    fontSize: 14,
-    body1: {
-      fontFamily: "Montserrat",
-      fontSize: 14
+    fontSize: 16,
+    h3: {
+      fontSize: "2.5rem",
+      "@media (max-width:900px)": {
+        fontSize: "1.5rem"
+      },
+      fontWeight: 800
     },
-    body2: {
-      fontFamily: "Open Sans",
-      fontSize: 40
+    h4: {
+      fontSize: "2.2rem",
+      color: "#ffffff",
+      fontWeight: 200
     },
-
     button: {
-      textTransform: "none",
-      fontWeight: "bold",
-      fontFamily: "Montserrat"
+      fontFamily: "Montserrat",
+      fontWeight: 900,
+      fontSize: "1.4rem",
+      "@media (max-width:900px)": {
+        fontSize: "1rem"
+      },
+      lineHeight: 2.22,
+      textTransform: "none"
     }
   },
+  spacing: 4,
   overrides: {
     MuiInput: {
       input: {
+        fontSize: "1.25rem",
+        "@media (max-width:900px)": {
+          fontSize: "1rem",
+          width: "18rem"
+        },
         fontWeight: "800",
         paddingTop: "2rem",
-        fontSize: "1rem",
-        width: "36rem",
+        width: "28rem",
+        height: "1rem",
         marginTop: "1rem"
       }
     },
     MuiInputLabel: {
       root: {
-        fontSize: "1.25rem"
-      },
-      outlined: {
-        "&$shrink": {
-          shrink: false
+        fontSize: "1.25rem",
+        "@media (max-width:900px)": {
+          fontSize: "0.85rem"
         }
       }
     }
-  },
-  palette: {
-    primary: { main: "#3A8DFF" },
-    secondary: { main: "#B0B0B0" }
   }
 });
