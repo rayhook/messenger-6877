38e18cf64c9638c1fa3bc3966324b814beb340cc
rayhook <koohyar@gmail.com>
2021-10-13 19:48:27 +0200

    Create attachment icon component

---
+++ client/src/resources/AttachIcon.js
@@ -0,0 +1,13 @@
+import { SvgIcon } from "@material-ui/core";
+export const AttachIcon = (props) => (
+  <SvgIcon {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke="currentColor">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+      />
+    </svg>
+  </SvgIcon>
+);
