d16a7519340495010bffc43ee23b9e940681a2e9
rayhook <koohyar@gmail.com>
2023-11-03 13:26:36 -0300

    Use axios instnace to make a request

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -4,9 +4,8 @@ import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { makeStyles } from "@material-ui/core/styles";
 // import { setActiveChat } from "../../store/activeConversation";
 import { connect } from "react-redux";
-import axios from "axios";
-
 import { ActiveChatContext } from "../../context/activeChat";
+import { axiosInstance } from "../../API/axiosConfig";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,81 +21,41 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const Chat = ({ convoId, username }) => {
+const Chat = ({ convoId, username, email }) => {
   const classes = useStyles();
   const { activeChat, setActiveChat } = useContext(ActiveChatContext);
 
-  const APIURL = "http://127.0.0.1:8000/messenger/";
-
-  const axiosInstance = axios.create({
-    baseURL: APIURL,
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("access")}`
-    }
-  });
-
-  axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-      const originalRequest = error.config;
-      if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
+  const handleSelectConversation = async () => {
+    try {
+      if (!email) {
+        setActiveChat({
+          ...activeChat,
+          conversationId: convoId
+        });
+      } else {
         try {
-          const res = await axios.post(APIURL + "api/token/refresh/", {
-            refresh: localStorage.getItem("refresh")
-          });
-          if (res.status === 200) {
-            localStorage.setItem("access", res.data.access);
-            axiosInstance.defaults.headers["Authorization"] = `Bearer ${res.data.access}`;
-            originalRequest.headers["Authorization"] = `Bearer ${res.data.access}`;
+          const response = await axiosInstance.post("conversation/create");
+          if (response.status === 201) {
+            setActiveChat({
+              ...activeChat,
+              conversationId: response.data.conversation_id
+            });
           }
-          return axiosInstance(originalRequest);
-        } catch (refreshError) {
-          console.error("Token refresh failed", refreshError);
-          return Promise.reject(refreshError);
+        } catch (error) {
+          console.error("failed to create a conversation", error);
         }
       }
-      return Promise.reject(error);
+    } catch (error) {
+      console.error(error);
     }
-  );
-
-  const handleGetConversations = async () => {};
-  // const handleCreateConversation = async () => {
-  //   try {
-  //     const response = await axiosInstance.post("/conversation/create", { username });
-  //     if (response.status === 201) {
-  //       setActiveChat({
-  //         username,
-  //         conversationId: response.data.conversation_id
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  const handleClick = async (conversation) => {
-    // await props.setActiveChat(conversation.otherUser.username);
   };
 
   return (
-    <Box className={classes.root} onClick={handleGetConversations}>
+    <Box className={classes.root} onClick={handleSelectConversation}>
       <BadgeAvatar username={convoId} online="true" sidebar={true} />
       <ChatContent username={username} />
     </Box>
   );
 };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     setActiveChat: (id) => {
-//       dispatch(setActiveChat(id));
-//     }
-//   };
-// };
-
-// export default connect(null, mapDispatchToProps)(Chat);
 
 export default Chat;
