b81df472fb1080a9d88b26164565ebc545496070
rayhook <koohyar@gmail.com>
2023-11-28 10:20:58 -0400

    Put related paths together

--- server_django/mybackend/messenger/urls.py
+++ server_django/mybackend/messenger/urls.py
@@ -7,17 +7,13 @@ from rest_framework_simplejwt.views import (
 from . import views
 
 urlpatterns = [
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("messages/", views.MessageView.as_view(), name="messages"),
-    path(
-        "search/",
-        views.SearchView.as_view(),
-        name="search",
-    ),
-    path("update/messages/", views.NewMessage.as_view(), name="last_message"),
-    path("conversations/", views.ConversationView.as_view(), name="conversations"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("conversations/", views.ConversationView.as_view(), name="conversations"),
+    path("search/", views.SearchView.as_view(), name="search"),
+    path("update/messages/", views.NewMessage.as_view(), name="update_message"),
+    path("messages/", views.MessageView.as_view(), name="messages"),
 ]
