e9da8386b2d74d6bb5ea8226ec1b6fbea4430acb
rayhook <koohyar@gmail.com>
2023-12-16 14:42:44 -0400

    Fix test to check message is correctly created

--- server_django/project/messenger/tests/tests.py
+++ server_django/project/messenger/tests/tests.py
@@ -7,9 +7,9 @@ class TestModels(TestCase):
     def test_message_creation(self):
         conversation = ConversationFactory()
 
-        message = MessageFactory
+        message = MessageFactory(conversation=conversation)
 
-        # assertion
+        # assertions
         self.assertIsNotNone(message.id)
         self.assertEqual(message.conversation, conversation)
         self.assertTrue(message.text)
