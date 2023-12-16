1e5df66c573d98a00c3a5d0e7af1dd84956942e7
rayhook <koohyar@gmail.com>
2023-12-16 14:43:45 -0400

    Fi factories to correctly accept models

--- server_django/project/messenger/tests/factories.py
+++ server_django/project/messenger/tests/factories.py
@@ -1,11 +1,11 @@
 import factory
 from django.contrib.auth.models import User
-from .models import Conversation, Message
+from ..models import Conversation, Message
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model: User
+        model = User
 
     username = factory.sequence(lambda n: f"user{n}")
     email = factory.LazyAttribute(lambda o: f"{o.username}@example.com")
@@ -14,7 +14,7 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 class ConversationFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model: Conversation
+        model = Conversation
 
     user1 = factory.SubFactory(UserFactory)
     user2 = factory.SubFactory(UserFactory)
@@ -22,8 +22,8 @@ class ConversationFactory(factory.django.DjangoModelFactory):
 
 class MessageFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model: Message
+        model = Message
 
-    Conversation = factory.SubFactory(ConversationFactory)
+    conversation = factory.SubFactory(ConversationFactory)
     text = factory.Faker("sentence")
     user = factory.SubFactory(UserFactory)
