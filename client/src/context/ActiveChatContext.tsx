297c7ca560ab2641a934086e4e5cffdb91f46960
rayhook <koohyar@gmail.com>
2024-02-09 08:16:26 -0400

    Define types for ActveChat state

--- client/src/context/ActiveChatContext.tsx
+++ client/src/context/ActiveChatContext.tsx
@@ -1,10 +1,25 @@
 import React, { createContext, useState } from "react";
 import { ProviderProps } from "../types";
 
-const ActiveChatContext = createContext(undefined);
+interface ActiveChatState {
+  username: null | string; // define types here
+  conversations: []; // define types here
+  newContacts: []; // define types here
+  conversationId: null; // define types here
+  messages: []; // define types here
+  users: []; // define types here
+  user2: null; // define types here
+  lastMessageId: null;
+  lastConversationId: null;
+}
 
-function ActiveChatProvider({ children }: ProviderProps) {
-  const [activeChat, setActiveChat] = useState({
+interface ActiveChatContextType {
+  activeChat: ActiveChatState;
+  setActiveChat: React.Dispatch<React.SetStateAction<ActiveChatState>>;
+}
+
+const defaultActiveChatState: ActiveChatContextType = {
+  activeChat: {
     username: null,
     conversations: [],
     newContacts: [],
@@ -14,7 +29,14 @@ function ActiveChatProvider({ children }: ProviderProps) {
     user2: null,
     lastMessageId: null,
     lastConversationId: null
-  });
+  },
+  setActiveChat: () => {}
+};
+
+const ActiveChatContext = createContext<ActiveChatContextType>(defaultActiveChatState);
+
+function ActiveChatProvider({ children }: ProviderProps) {
+  const [activeChat, setActiveChat] = useState(defaultActiveChatState.activeChat);
 
   return (
     <ActiveChatContext.Provider value={{ activeChat, setActiveChat }}>
